172b6f4ba2d003da74b44989bb39addbf417444b
rangurari <rangurari@gmail.com>
2020-12-30 16:12:28 +0000

    na

--- src/learn1/main.py
+++ src/learn1/main.py
@@ -3,3 +3,28 @@ print("hello ran")
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
+
+print("----------------------------------")
+
+#print(2 + 19)
+print(10 % 4)
+
+
+print("----------------------------------")
+
+ran_age = 11
+amit_age = 8
+difference = ran_age - amit_age
+ran_age = 20
+
+difference = ran_age - amit_age
+print(difference)
+
+
+ran_name = 'ran'
+amit_name = "amit"
+print(ran_name, amit_name)
+
+kid_names = 'sahar\nran\namit'
+print(kid_names)
+
